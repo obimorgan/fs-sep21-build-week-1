296018ad726b798c78c50860660874dfe14222ed
emyriounis <el.myrioun@gmail.com>
2021-10-19 19:58:04 +0300

    fixing nav scroll bug

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let visibility = true;
 const navVisibility = () => {
   let nav = document.querySelector("nav");
+  let main = document.querySelector("main");
 
   visibility ? nav.classList.add("d-none") : nav.classList.remove("d-none");
+  visibility ? main.classList.remove("d-none") : main.classList.add("d-none");
 
   visibility = !visibility;
 };
