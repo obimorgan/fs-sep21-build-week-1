8315bcbb8e2d95b8813bc422139a957994fbf89e
Roby Morgan <roby.c.morgan@gmail.com>
2021-10-26 16:57:12 +0200

    Only added api data.

--- artist-page/index.js
+++ artist-page/index.js
@@ -1,3 +1,34 @@
+window.onload = () => {
+  fetchEminem()
+}
+
+const fetchEminem = () => {
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+      "x-rapidapi-key": "f0aa699c5fmsh28554b7069ba793p1dc7d9jsnf8d185b3f84f"
+    }
+  })
+  .then((res) => res.json())
+  .then(data => {
+    console.log(data)
+  })
+  .then(data => {
+    const albumList = document.querySelector('.album-list')
+    const albumListBtn = document.createElement('button')
+    albumListBtn.classList.add('dropdown-item.bg-dark.text-light')
+    albumListBtn.innerText = data.data[0].album.title
+  })
+  .catch(err => {
+    console.error(err);
+  });
+  // fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem')
+  // .then((res) => res.json())
+  // .then(data => console.log(data))
+  // .catch((e) => console.log(e))
+}
+
 let visibility = true;
 const navVisibility = () => {
   let nav = document.querySelector("nav");
